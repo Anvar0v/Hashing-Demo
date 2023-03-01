2a492b14ab6ddf296462553fbd2ba0a123a63061
abuUsman_04 <i123l183l@gmail.com>
2023-03-01 12:25:06 +0300

    Add project files.

---
+++ Hashing/Models/SodiumLibrary.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hashing.Models;
+public class SodiumLibrary
+{
+    const int key_size = 64;
+    const int iterations = 350000;
+    HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA384;
+
+    public string HashPassword(string password,out byte[] salt)
+    {
+        salt = RandomNumberGenerator.GetBytes(key_size);
+
+        var hash = Rfc2898DeriveBytes.Pbkdf2(
+            Encoding.UTF8.GetBytes(password),
+            salt,
+            iterations,
+            hashAlgorithm,
+            key_size
+            );
+
+        return Convert.ToHexString(hash);
+    }
+    public bool VerifyPassword(string password, string hash, byte[] salt)
+    {
+        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, hashAlgorithm, key_size);
+        return hashToCompare.SequenceEqual(Convert.FromHexString(hash));
+    }
+}
---
+++ Hashing/Program.cs
@@ -0,0 +1,12 @@
+﻿using Hashing.Models;
+
+var sodiumLib = new SodiumLibrary();
+
+var hash = sodiumLib.HashPassword("my_password", out var salt);
+
+Console.WriteLine($"Hashed Password = {hash}");
+Console.WriteLine($"Generated salt = {Convert.ToHexString(salt)}");
+
+var isSuccess = sodiumLib.VerifyPassword("my_password", hash, salt);
+
+Console.WriteLine($"Final Result = {isSuccess}");
